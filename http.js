128723618f90e626304be5a5679f56c059200658
yaopiaohai <yaopiaohai@corp.netease.com>
2013-03-20 15:00:12 +0800

    misc:refactor http

--- http.js
+++ http.js
@@ -1,44 +1,25 @@
 var http = require('http');
 var envConfig = require('./app/config/env.json');
 var config = require('./app/config/'+envConfig.env+'/config');
-var Robot = require('./lib/robot').Robot;
-var fs = require('fs');
-var robots = [];
-//
-// node main master run service
-//
-var run = function(num) {
+
+var run = function(num){
+    var args = " node app.js client ";
     for (var i = 0 ;i < num;i++) {
-        var robot = new Robot(config);
-        var path = __filename.substring(0,__filename.lastIndexOf('/'));
-        var scriptFile = path + envConfig.script;
-        robot.runAgent(scriptFile);
-        robots.push(robot);
+        require('child_process').exec(args, function(err, data){});
     }
 }
 
-// Controlling server.
+var stop = function() {
+    var args = "kill -9 `ps -ef|grep node | grep -v http.js| grep -v grep |  awk '{print $2}'`"
+    require('child_process').exec(args, function(err, data){});
+}
+
 http.createServer(function (req, res) {
     if (req.method === "GET") {
         var url = require('url').parse(req.url, true);
-
         if (url.pathname === '/') {
-            // Return stats on '/'
             return res.end(JSON.stringify(config) + "\n");
-        }  if (url.pathname === '/stats') {
-            // Return stats on '/'
-            try {
-                var actors = {};
-                for (var i=0;i<robots.length;i++){
-                    actors = robots[i].agent.actors || {};
-                    break;
-                }
-                var stats = {size:robots.length,actors:actors};
-                return res.end(JSON.stringify(stats) + "\n");
-            } catch(ex) {
-                return res.end(JSON.stringify(ex.stack) + "\n");
-            }
-        } else if (url.pathname === '/set') {
+        }  else if (url.pathname === '/set') {
             // Set params on '/set', preserving the type of param.
             for (var key in url.query) {
                 config['apps'][key] = (typeof config[key] == 'number') ? +url.query[key] : url.query[key];
@@ -52,7 +33,7 @@ http.createServer(function (req, res) {
             require('child_process').exec("cd /home/ubuntu/hello && git pull ", function() {});
             return res.end("OK\n");
         } else if (url.pathname === '/stop') {
-            setTimeout(function(){process.exit(0)},1000);
+            setTimeout(function(){stop()},1000);
             return res.end("HTTP SERVER CLOSE OK\n");
         } else if (url.pathname === '/reset') {
             return res.end(ok);
@@ -70,5 +51,5 @@ console.log(' http server start at port '  + config.master.cwebport)
 
 process.on('uncaughtException', function(err) {
   console.error(' Caught exception: ' + err.stack);
-  fs.writeFileSync('/tmp/log',err.stack,'utf8');
+  require('fs').writeFileSync('/tmp/log',err.stack,'utf8');
 });
